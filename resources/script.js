1f41f59f94d494979493ed0959d46aad542a8ff0
Kevin Liang <36863902+Sanbroh@users.noreply.github.com>
2021-01-23 01:54:58 -0500

    Add files via upload

---
+++ resources/script.js
@@ -0,0 +1,42 @@
+inRoom = true;
+
+function exitRoom() {
+  if (inRoom) {
+    inRoom = false;
+    document.getElementById("topnav-status-dot").style.color = "black";
+    document.getElementById("topnav-room").innerHTML = "No Room";
+  }
+}
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
